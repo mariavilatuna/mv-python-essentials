d26b56583bf5e6c005b206e889cee0135cf06c2e
Mafer <maferv@Mafers-MacBook-Pro.local>
2022-10-04 00:45:41 -0500

    Primeros scripts curso python essential

---
+++ deber01_uso_input_maria_vilatuna.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 29 20:25:57 2022
+
+@author: maferv
+"""
+print("Formulario de registro Emprea Horizontes")
+print("El presente formulario corresponde al ingreso de información personal para el registro del curso de Design thinking")
+print("Favor ingresar la información, los campos requeridos  requerida")
+nombre=input("Ingrese nombre: ")
+apellido=input("Ingrese apellido: ")
+ubicacion=input("Ingrese ubicación: ")
+edad=input("Ingrese edad: ")
+espacio= "  "
+print("Muchas gracias"+espacio+nombre+espacio+apellido+espacio+"su edad es"+espacio+edad+espacio+"su ubicación es"+espacio+ubicacion)
+print("Su registro ha sido exitoso")
+
+
+
+
